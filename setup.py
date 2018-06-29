3c174b0c6bbbe9ea67e2447bb956700262be465d
Justinas Stankevicius <justinas@justinas.org>
2018-06-29 22:15:46 +0300

    Add a minimalist setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'pelican-podcast-feed',
+    version = '0.0.1',
+    url = 'https://github.com/magnunleno/pelican-podcast-feed',
+    author = 'Magnun Leno',
+    author_email = 'magnun.leno@gmail.com',
+    description = 'This plugins adds a feed generator and feed writer for your podcast.',
+    packages = find_packages(),
+    install_requires = [
+        'six',
+        'pelican',
+        'jinja2',
+        'feedgenerator',
+    ],
+)
