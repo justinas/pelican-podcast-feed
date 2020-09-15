3d4f110f743a090e103876df9756424371c88e5a
Justinas Stankevicius <justinas@justinas.org>
2020-09-15 19:51:30 +0300

    Add content namespace

--- pelican_podcast_feed.py
+++ pelican_podcast_feed.py
@@ -58,6 +58,7 @@ class PodcastFeed(Rss201rev2Feed):
         """
         attrs = super(PodcastFeed, self).root_attributes()
         attrs['xmlns:itunes'] = "http://www.itunes.com/dtds/podcast-1.0.dtd"
+        attrs['xmlns:content'] = "http://purl.org/rss/1.0/modules/content/"
         attrs['version'] = '2.0'
         return attrs
 
